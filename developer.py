5cdb0bce313e52ac18d2afa0ccf4bf77d34da005
ddoiy1 <90036352+ddoiy1@users.noreply.github.com>
2021-11-08 16:09:22 +0900

    Add files via upload

---
+++ developer.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('{0.user} 봇을 연결했습니다'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("~투표"):
+        vote = message.content[4:].split("/")
+        await message.channel.send("투표 - " + vote[0])
+        for i in range(1, len(vote)):
+                choose = await message.channel.send("```" + vote[i] + "```")
+                await choose.add_reaction('👍')
+
+
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
+
